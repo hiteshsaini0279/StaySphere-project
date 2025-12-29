a44fd7fce21d8b8ac88c6f8adde5c1cb86d3aaa5
Hitesh saini <hiteshsaini0277@gmail.com>
2025-12-29 16:03:08 +0530

    phase 2 second last day

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const methodOverride=require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session =require("express-session");
-const listings= require("./routes/listing.js");
-const reviews= require("./routes/review.js");
 const flash= require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
 const User=require("./models/user.js");
+
+const listingRouter= require("./routes/listing.js");
+const reviewRouter= require("./routes/review.js");
+const userRouter= require("./routes/user.js");
+
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 async function main(){
     await mongoose.connect(MONGO_URL);
@@ -61,8 +64,9 @@ app.use((req,res,next)=>{
     res.locals.error=req.flash("error");
     next();
 });
-app.use("/listings", listings);
-app.use("/listings/:id/reviews" ,reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews" ,reviewRouter);
+app.use("/",userRouter);
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page not found"));
--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
 const { required } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose= require("passport-local-mongoose");
+const passportLocalMongoose= require("passport-local-mongoose").default;
 
 
-const userSchema= new schema({
+const userSchema= new Schema({
     email:{
         type:String,
-        required:true
+        required:true,
     }
 });
 
