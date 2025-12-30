d83d4f2d7f711434490613077a0d7d4ffb2cd843
Hitesh saini <hiteshsaini0277@gmail.com>
2025-12-30 15:14:20 +0530

    authentication phase

--- routes/listing.js
+++ routes/listing.js
@@ -4,8 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { listingSchema } = require("../schema.js");
 const Listing=require("../models/listing.js");
-
-
+const { isLoggedIn } = require("../middleware.js");
 
 
 
@@ -30,7 +29,7 @@ router.get("/", wrapAsync (  async (req,res) =>{
 }));
 
 ///NEW ROUTE
-router.get("/new", (req,res)=>{
+router.get("/new", isLoggedIn, (req,res)=>{
     res.render("listings/new.ejs"); 
 });
 
@@ -47,7 +46,7 @@ router.get("/:id",  wrapAsync ( async (req,res)=>{
 }));
 
 ///CREATE NEW ROUTE
-router.post("/", validateListing,
+router.post("/", isLoggedIn,validateListing,
   wrapAsync(async (req,res) => {
     const newListing = new Listing(req.body.listing); 
     await newListing.save();
@@ -58,7 +57,7 @@ router.post("/", validateListing,
 );
 
 ///edit ROUTE
-router.get("/:id/edit",  wrapAsync ( async (req,res)=>{
+router.get("/:id/edit", isLoggedIn, wrapAsync ( async (req,res)=>{
     let {id}= req.params;
     const listing =await Listing.findById(id);
     if(!listing){
@@ -69,7 +68,7 @@ router.get("/:id/edit",  wrapAsync ( async (req,res)=>{
 }));
 
 ///update ROUTE
-router.put("/:id", validateListing, wrapAsync ( async (req, res) => {
+router.put("/:id",isLoggedIn, validateListing, wrapAsync ( async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, req.body.listing);
       req.flash("success", " Listing updated!!");
@@ -77,7 +76,7 @@ router.put("/:id", validateListing, wrapAsync ( async (req, res) => {
 }));
 
 ///delete ROUTE
-router.delete("/:id", wrapAsync (  async (req, res) => {
+router.delete("/:id", isLoggedIn,wrapAsync (  async (req, res) => {
     let { id } = req.params;
   let deletedListing=  await Listing.findByIdAndDelete(id);
     req.flash("success", "Listing deleted!!");
--- routes/user.js
+++ routes/user.js
@@ -29,6 +29,17 @@ router.get("/login", (req, res)=>{
 router.post("/login",passport.authenticate("local", {failureRedirect :"/login", faiureFlash:true,}), async(req, res)=>{
 req.flash("success","Welcome back to StaySphere!!!")
 res.redirect("/listings");
+});
+
+
+router.get("/logout", (req, res,next)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","Logged out successfully!!!");
+        res.redirect("/listings");
+    });
 })
 
 module.exports=router;
