d07bd8f2c9213d8aade2e04a9c4d480326ff8c9b
Hitesh saini <hiteshsaini0277@gmail.com>
2026-01-09 20:12:05 +0530

    phase 3b day3

--- public/js/map.js
+++ public/js/map.js
@@ -3,6 +3,6 @@
       container: 'map',
       style: "base-v4",
       center: [75.79, 26.91], // starting position [lng, lat]
-      zoom: 1   // starting zoom
+      zoom: 10   // starting zoom
     });
  
