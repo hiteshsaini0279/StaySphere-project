2b49a7624d72b24a01a92d90979eecaff090ac00
Hitesh saini <hiteshsaini0277@gmail.com>
2026-01-10 13:34:12 +0530

    phase 3 day9

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
- maptilersdk.config.apiKey = mapToken;
-    const map = new maptilersdk.Map({
+ mapboxgl.accessToken = mapToken;
+    const map = new mapboxgl.Map({
       container: 'map',
-      style: "base-v4",
+      style: "mapbox://styles/mapbox/streets-v12",
       center: [75.79, 26.91], // starting position [lng, lat]
       zoom: 10   // starting zoom
     });
